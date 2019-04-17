5fc8f04da32f11cd43b4eb601608274417b876b6
chinmoy <chinmoycse03@gmail.com>
2019-04-17 15:51:15 -0500

    update

--- day 12/src/app/app.component.ts
+++ day 12/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,Input,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `Counter Component <app-counter></app-counter>`,
+  template: `
+      Counter Component <app-counter  [childVal]="parentVal" (counterChange)="receiveChildVal($event)"></app-counter>
+      <p>Component Counter Value= {{currentCounterValue}}</p>`,
   styles: [``]
 })
 export class AppComponent {
-  title = 'Heros';
+  title = 'Lab 1';
+  parentVal:number=20;
+  currentCounterValue:number;
+  receiveChildVal($event){
+    console.log($event);
+    this.currentCounterValue=$event;
+  }
 }
--- day 12/src/app/counter.component.ts
+++ day 12/src/app/counter.component.ts
@@ -1,23 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter,Input } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   template: `
     <button (click)="increment()">+</button>
-    <span>{{counterVal}}</span>
+    <span #componentCounterValue>{{counterVal}}</span>
     <button (click)="decrement()">-</button>
   `,
   styles: []
 })
-export class CounterComponent  {
-
+export class CounterComponent {
+  componentCounterValue:number
   counterVal=1;
+  @Input() childVal: number;
+  ngOnInit(){
+    this.counterVal=this.childVal;
+  }
   increment(){
     console.log("increment");
     this.counterVal++;
+    this.counterChange.emit(this.counterVal);
   }
   decrement(){
     console.log("decrement");
     this.counterVal--;
+    this.counterChange.emit(this.counterVal);
   }
+ 
+  @Output () counterChange = new EventEmitter();
 }
